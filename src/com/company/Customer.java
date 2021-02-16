0460524a7691e77968b99baa54043eeaff4b85ff
Volkanico <vcekmez15181@alumnes.iesmanacor.cat>
2021-02-16 12:13:58 +0100

    Initial commit

---
+++ src/com/company/Customer.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Customer {
+
+    private boolean isVip;
+    private boolean isSpecial;
+    private int type;
+
+    public static final int NORMAL = 0;
+    public static final int SPECIAL = 1;
+    public static final int VIP = 2;
+
+    public Customer(boolean isVip, boolean isSpecial, int type) {
+        this.isVip = isVip;
+        this.isSpecial = isSpecial;
+        this.type = type;
+    }
+
+    public double applyDiscount(double price, double discount) {
+        return Discount.applyDiscount();
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public boolean isVip() {
+        return isVip;
+    }
+
+    public boolean isSpecial() {
+        return isSpecial;
+    }
+}
---
+++ src/com/company/Student.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Student {
+    public float evaluateTerm(float homeworkMark, float examMark, float attitude) {
+        float examMarkDef = examMark;
+        float homeWorkDef = homeworkMark;
+
+        if (examMark < 5) {
+            examMarkDef = 1;
+        }
+
+        if (homeworkMark < 4) {
+            homeWorkDef = 1;
+        }
+        return (homeWorkDef + examMarkDef) / 2 + attitude;
+    }
+}
